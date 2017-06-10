6890f62fdee2c9a388bb99ea85b4f48328a1b138
wdlkmpx <wdlkmpx@gmail.com>
2017-06-10 13:48:11 +0800

    simplify: do not require mate-common, caja, nls, etc..

--- autogen.sh
+++ autogen.sh
@@ -1,29 +1,14 @@
 #!/bin/sh
-# Run this to generate all the initial makefiles, etc.
 
-srcdir=`dirname $0`
-test -z "$srcdir" && srcdir=.
+#Get all required m4 macros required for configure
+libtoolize -ci
+aclocal
 
-PKG_NAME="engrampa"
+#generate configure
+autoconf
 
-(test -f $srcdir/configure.ac) || {
-    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
-    echo " top-level $PKG_NAME directory"
-    exit 1
-}
+#Generate config.h.in
+autoheader
 
-which mate-autogen || {
-    echo "You need to install mate-common"
-    exit 1
-}
-
-which yelp-build || {
-    echo "You need to install yelp-tools" 
-    exit 1
-}
-
-REQUIRED_AUTOMAKE_VERSION=1.10
-REQUIRED_GTK_DOC_VERSION=1.13
-USE_MATE2_MACROS=1
-
-. mate-autogen
+#Generate Makefile.in's
+automake -ac
